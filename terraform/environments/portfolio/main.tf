04b5c34967fde0b530cc9725f0c6bd13d881659c
Mateusz Majkusiak <mateusz.majkusiak.ltsf@gmail.com>
2026-09-22 20:31:50 +0200

    feat(network): add private services access

--- terraform/environments/portfolio/main.tf
+++ terraform/environments/portfolio/main.tf
@@ -27,6 +27,9 @@ module "network" {
   pods_cidr     = var.network_cidrs.pods
   services_cidr = var.network_cidrs.services
 
+  private_services_access_enabled = true
+  private_services_prefix_length  = 24
+
   depends_on = [
     module.project_services
   ]
